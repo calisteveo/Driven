a0955f75cc08faf3822b3ef0bd770a0dffa3348c
calisteveo <soglesbee1@gmail.com>
2014-08-19 13:09:30 -0700

    created friendship model and controller and set up user/friendship relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Driven::Application.routes.draw do
+  get "friendship/create"
+  get "friendship/destroy"
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
