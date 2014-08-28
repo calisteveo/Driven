53b5d9605ce6dd8816fcd512184ab4c4aa737466
calisteveo <soglesbee1@gmail.com>
2014-08-28 13:39:35 -0700

    work in progress to show user and add friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,4 @@
-class FriendshipController < ApplicationController
+class FriendshipsController < ApplicationController
   def create
     @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
     if @friendship.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Driven::Application.routes.draw do
-  get "friendship/create"
-  get "friendship/destroy"
+  resources :friendships
+  resources :users
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
-  get '/users/:id', to: 'users#index'
+  
   root to: "sessions#index"
 end
