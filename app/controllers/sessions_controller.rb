9c108fd5db83c6d07dee4b7bacc7a00b83b6af47
paninapress <npacifico25@gmail.com>
2014-09-02 14:43:36 -0700

    added navigation links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,16 @@
 class SessionsController < ApplicationController
   
   def index
-    
+    if current_user
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def create
     # raise env["omniauth.auth"].to_yaml
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_url
+    redirect_to user_path(user.id)
   end
 
   def destroy
